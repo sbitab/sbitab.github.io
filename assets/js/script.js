8a13d2981fad4ee37e5bd22cf160997f3a795779
sajjad <tarkhasi.sajjad@gmail.com>
2019-12-29 21:06:04 +0330

    Fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,8 @@ function staticLoadPlaces() {
     {
       name: 'Magnemite',
       location: {
-        lat: 44.496470,
-        lng: 11.320180,
+        lat: 37.392101,
+        lng: 57.916383,
       }
     },
   ];
@@ -24,7 +24,7 @@ function renderPlaces(places) {
 
     let model = document.createElement('a-entity');
     model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-    model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+    model.setAttribute('gltf-model', 'https://sbitab.github.io/ar/assets/magnemite/scene.gltf');
     model.setAttribute('rotation', '0 180 0');
     model.setAttribute('animation-mixer', '');
     model.setAttribute('scale', '0.5 0.5 0.5');
